6854a016230f1a943ca9ded820942fe91da42a28
Thihasoe <thihasoe.soelay@gmail.com>
2025-01-04 19:03:58 +0630

    about us photo & text place change

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -13,16 +13,16 @@ const Testimonials = () => {
         <div className="testimonials my-8" id='aboutus'>
         <div className="m-6 space-y-12 flex items-center justify-center">
           <div className="flex flex-col md:flex-row items-center justify-center gap-4 ">
+            <div className="md:w-1/2 text-center md:text-left space-y-4 flex justify-center flex-col">
+              <h2 className="text-3xl font-bold text-gray-300 text-center md:text-left">{whoWeAre.title}</h2>
+              <p className="text-gray-400 leading-relaxed text-center md:text-left">{whoWeAre.text}</p>
+            </div>
             <div className="md:w-1/2 p-10">
               <img
                 src={whoWeAre.image}
                 alt={whoWeAre.title}
                 className="rounded-lg shadow-lg w-full object-cover"
               />
-            </div>
-            <div className="md:w-1/2 text-center md:text-left space-y-4 flex justify-center flex-col">
-              <h2 className="text-3xl font-bold text-gray-300 text-center md:text-left">{whoWeAre.title}</h2>
-              <p className="text-gray-400 leading-relaxed text-center md:text-left">{whoWeAre.text}</p>
             </div>
           </div>
         </div>
