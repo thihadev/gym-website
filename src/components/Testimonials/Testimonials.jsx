1f82fd1087b88c01e5af49e126372d8371ebfee2
Thihasoe <thihasoe.soelay@gmail.com>
2025-01-04 19:01:19 +0630

    about us photo added

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
 import './Testimonials.css';
+
+import About from "../../assets/about2.jpg";
 
 const Testimonials = () => {
     const whoWeAre = {
         title: 'Who We Are',
         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        image: 'https://via.placeholder.com/400x300', // Replace with your image URL
+        image: About,
       };
     
       return (
